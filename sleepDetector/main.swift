b7a1dfa970f85da28c56ac20ecca1f5dbaf848ea
Joe Tennant <joetennant@gmail.com>
2018-12-31 14:25:50 -0500

    Initial Commit

---
+++ sleepDetector/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  sleepDetector
+//
+//  Created by Joseph Tennant on 12/31/18.
+//  Copyright © 2018 7SIGNAL. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
